f80212168de9fec2307fe36f672cf501fb7a6898
shencanet <santi.dionis@gmail.com>
2024-09-21 20:50:36 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { Fragment } from 'react';
+
 const App = () => {
-  console.log('App.jsx 2');
-  return <h1>shen App</h1>;
-};
+
+  
+  return (
+    <>
+    <h1>shen App</h1>
+    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="google" />
+    </>
+  );
+  };
+
 
 export default App;
