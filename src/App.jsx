fec26080b78597a2e9d9ab341248f348a1944634
shencanet <santi.dionis@gmail.com>
2024-09-22 20:51:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { Fragment } from 'react';
 
+const MyButton = () =>{
+  return <button>Click me!</button>
+}
 const App = () => {
 
-  const name = 'shen';
+  const name = 'Mi Titulo desde una constante';
   const classTitle = 'text-center';
   const patkImg = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
   
   return (
     <>
     <h1 className={classTitle}>{name.toUpperCase()}</h1>
-    <img src={patkImg} alt="google" />
+    <img src={patkImg} alt={`imagen-${name}`}/>
     </>
   );
   };
 
 
 export default App;
+
+//alt 96 comillas invertidas
