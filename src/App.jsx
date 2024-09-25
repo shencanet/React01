f7d821709025d97c3187ab7d49292e41b4b32acc
shencanet <santi.dionis@gmail.com>
2024-09-25 19:17:05 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,14 @@ const App = () => {
   const name = 'Mi Titulo desde una constante';
   const classTitle = 'text-center';
   const patkImg = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
-  const user = true;
+  const pathImg2 = "./src/assets/images/react.svg";
+  const user = false;
 
   return (
     <>
       <h1 className={classTitle}>{name.toUpperCase()}</h1>
       <img src={patkImg} alt={`imagen-${name}`} />
+      <img src={pathImg2} alt={`imagen-${name}`} />
       <MyButton />
       {user ? <OnlineText /> : <OfflineText />}
       {user && <OnlineText />}
