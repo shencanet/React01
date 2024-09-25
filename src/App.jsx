f76f3c267c909ec291f8d8a7f499c0d81480401f
shencanet <santi.dionis@gmail.com>
2024-09-25 19:04:09 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
 import { Fragment } from 'react';
 
-const MyButton = () =>{
-  return <button>Click me!</button>
-}
-const App = () => {
+const OnlineText = () => {
+  return (
+    <div>
+      <h1>Online</h1>
+    </div>
+  );
+};
 
+const OfflineText = () => {
+  return (
+    <div>
+      <h1>Offline</h1>
+    </div>
+  );
+};
+
+const MyButton = () => {
+  return <button>Click me!</button>;
+};
+
+const App = () => {
   const name = 'Mi Titulo desde una constante';
   const classTitle = 'text-center';
   const patkImg = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
-  
+  const user = true;
+
   return (
     <>
-    <h1 className={classTitle}>{name.toUpperCase()}</h1>
-    <img src={patkImg} alt={`imagen-${name}`}/>
+      <h1 className={classTitle}>{name.toUpperCase()}</h1>
+      <img src={patkImg} alt={`imagen-${name}`} />
+      <MyButton />
+      {user ? <OnlineText /> : <OfflineText />}
+      {user && <OnlineText />}
+
     </>
   );
-  };
-
+};
 
 export default App;
 
